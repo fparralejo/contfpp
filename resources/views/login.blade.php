450487c164e8e4c88a322a51c14820bee10f5090
olimpiada <fparralejo1970@gmail.com>
2016-04-23 20:41:09 +0200

    Presupuestos: He probado a quitar el PHPMailer mio y dejar el de vendor pero no funciona, asi que se queda como está

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -47,9 +47,9 @@
         <div class="container well" id="sha">
             <div class="row">
                 <div class="col-xs-12">
-                    <span><center><h3>Contabilidad</h3></center></span>	
-                    <span><center><h5>Versión 0.1</h5></center></span>	
-                    <span><center><h6>19/04/2016</h6></center></span>	
+                    <span><center><h3>Contabilidad FPP</h3></center></span>	
+                    <span><center><h5>Versión 0.20160423</h5></center></span>	
+                    <span><center><h6>23/04/2016</h6></center></span>	
                     <br/>
                 </div>
             </div>
